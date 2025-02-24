5108e3c3d89ed0fb3513f294c093601ead06505e
Arundhati Singh <arundhatisingh171003@gmail.com>
2025-02-24 01:58:02 -0600

    updated vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  preview: {
+    host: true, 
+    port: 4173,
+    allowedHosts: ["bucky-bulletin-54cf84e13b3f.herokuapp.com"]
+  },
+  server: {
+    host: true,
+    port: process.env.PORT || 4173
+  }
+});
